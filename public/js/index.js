5dd9b3b0165e42ddb6a5e0fd2b443361c7e833d7
testtesttesttest45 <weilinquek201@gmail.com>
2023-09-14 08:50:18 +0800

    "cant go to ocean" text

--- public/js/index.js
+++ public/js/index.js
@@ -12,30 +12,59 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function create() {
         const width = this.sys.game.config.width;
         const height = this.sys.game.config.height;
-    
+
         const land = this.add.image(width / 2, height / 2, 'land').setInteractive();
-    
+
         const originalWidth = land.width;
         const originalHeight = land.height;
-    
+
         land.setDisplaySize(width, height);
-    
+
+        this.messageText = this.add.text(width / 2, height / 2, '', {
+            font: '22px Orbitron',
+            fill: '#ff0000',
+            align: 'center'
+        });
+        this.messageText.setOrigin(0.5, 0.5);
+        this.messageText.setVisible(false);
+
         this.input.on('pointerdown', function (pointer) {
             const x = Math.round((pointer.x - land.x + width / 2) * (originalWidth / width));
             const y = Math.round((pointer.y - land.y + height / 2) * (originalHeight / height));
-    
+
             // Constrain x and y to the dimensions of the texture
             const constrainedX = Math.max(0, Math.min(x, originalWidth - 1));
             const constrainedY = Math.max(0, Math.min(y, originalHeight - 1));
-    
+
             const color = this.textures.getPixel(constrainedX, constrainedY, 'land');
-    
+
             if (color.alpha !== 0) {
                 const hexColor = rgbToHex(color.red, color.green, color.blue);
-                console.log('Hex color:', hexColor);
-            
+                // console.log('Hex color:', hexColor);
+
                 if (color.blue > 155) {
                     console.log("This area is ocean");
+
+                    let x = Phaser.Math.Clamp(pointer.x, this.messageText.width / 2, this.sys.game.config.width - this.messageText.width / 2); // ensure the text is within the canvas
+                    let y = Phaser.Math.Clamp(pointer.y, this.messageText.height / 2, this.sys.game.config.height - this.messageText.height / 2);
+                    // The minimum x and y are set to half of the text's width and height, respectively, to prevent the text from going off the left and top edges of the screen.
+                    
+                    this.messageText.setPosition(x, y);
+                    this.messageText.setText("Can't go there!");
+                    this.messageText.setVisible(true);
+
+                    this.tweens.add({
+                        targets: this.messageText,
+                        alpha: { start: 0, to: 1 },
+                        duration: 500,
+                        ease: 'Linear',
+                        yoyo: true, // tween play in reverse/fade-out effect
+                        repeat: 0,
+                        onComplete: () => {
+                            this.messageText.setVisible(false);
+                            this.messageText.setAlpha(1); // Reset alpha value for the next use
+                        }
+                    });
                 } else {
                     console.log("This area is land");
                 }
