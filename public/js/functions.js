21dafe819ea763fea2f9f0ad9aa634eb9cbae9fb
testtesttesttest45 <weilinquek201@gmail.com>
2023-09-17 09:22:30 +0800

    A* Algorithm to move on shorelines

---
+++ public/js/functions.js
@@ -0,0 +1,85 @@
+export function rgbToHex(r, g, b) {
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+
+    if (r.length == 1)
+        r = "0" + r;
+    if (g.length == 1)
+        g = "0" + g;
+    if (b.length == 1)
+        b = "0" + b;
+
+    return "#" + r + g + b;
+}
+
+export function resize(game) {
+    const battleScene = document.getElementById('battle-scene');
+    const gameWidth = game.config.width;
+    const gameHeight = game.config.height;
+
+    // Set the canvas dimensions to be exactly the same as the container dimensions
+    game.canvas.style.width = battleScene.offsetWidth + 'px';
+    game.canvas.style.height = battleScene.offsetHeight + 'px';
+
+    // Remove the margin settings to ensure the canvas fully covers the container
+    game.canvas.style.marginTop = '0px';
+    game.canvas.style.marginLeft = '0px';
+}
+
+export function gamePause(isPaused, game) {
+    const resumeButton = document.querySelector('.resume-button');
+    
+    if (!game || game.scene.scenes[0].sceneName !== "battle-scene") {
+        console.warn('Pause function is not applicable for the current scene');
+        return;
+    }
+
+    if (isPaused) {
+        game.loop.pause();
+        if (game.scene.scenes[0].scene.isActive()) {  // Ensure the scene is active before pausing
+            game.scene.scenes[0].scene.pause();
+        }
+        game.scene.scenes[0].tweens.pauseAll();  // Pause all active tweens
+        game.scene.scenes[0].pauseText.setVisible(true);
+        game.scene.scenes[0].input.enabled = false;
+        resumeButton.style.display = 'block';
+    } else {
+        game.loop.resume();
+        if (!game.scene.scenes[0].scene.isActive()) {  // Ensure the scene is paused before resuming
+            game.scene.scenes[0].scene.resume();
+        }
+        game.scene.scenes[0].tweens.resumeAll();  // Resume all active tweens
+        game.scene.scenes[0].input.enabled = true;
+    }
+}
+
+export function isValidPath(startX, startY, endX, endY, originalWidth, originalHeight, width, height, textures) { // based on Bresenham's line algorithm
+    let x1 = Math.round(startX * (originalWidth / width));
+    let y1 = Math.round(startY * (originalHeight / height));
+    let x2 = Math.round(endX * (originalWidth / width));
+    let y2 = Math.round(endY * (originalHeight / height));
+
+    let dx = Math.abs(x2 - x1); // find the absolute value of the difference between the x-coordinates of the two points
+    let dy = Math.abs(y2 - y1);
+
+    let sx = (x1 < x2) ? 1 : -1; // increment or decrement the x and y values as we step along the line. If x1 is less than x2, we increment x1 by 1, otherwise we decrement it by 1
+    let sy = (y1 < y2) ? 1 : -1;
+
+    let err = dx - dy; // to determine when we need to step in the y direction (as opposed to the x direction)
+
+    while (true) {
+        const color = textures.getPixel(x1, y1, 'land');
+
+        if (color.blue > 155) {
+            return false;
+        }
+
+        if (x1 === x2 && y1 === y2) break; // we've reached the end of the line
+        let e2 = 2 * err; // calculate the error value for the next step
+        if (e2 > -dy) { err -= dy; x1 += sx; } // if the error is greater than -dy, we step in the x direction
+        if (e2 < dx) { err += dx; y1 += sy; } // if the error is less than dx, we step in the y direction
+    }
+
+    return true;
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-import { rgbToHex } from './test.js';
+import { rgbToHex, resize, gamePause, isValidPath } from './functions.js';
 document.addEventListener('DOMContentLoaded', (event) => {
     const gameScreen = document.getElementById('game-screen');
     const battleScene = document.getElementById('battle-scene');
@@ -11,36 +11,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     function preload() {
         this.load.image('land', 'assets/images/land.png');
-    }
-
-    function isValidPath(startX, startY, endX, endY, originalWidth, originalHeight, width, height, textures) { // based on Bresenham's line algorithm
-        let x1 = Math.round(startX * (originalWidth / width));
-        let y1 = Math.round(startY * (originalHeight / height));
-        let x2 = Math.round(endX * (originalWidth / width));
-        let y2 = Math.round(endY * (originalHeight / height));
-
-        let dx = Math.abs(x2 - x1); // find the absolute value of the difference between the x-coordinates of the two points
-        let dy = Math.abs(y2 - y1);
-
-        let sx = (x1 < x2) ? 1 : -1; // increment or decrement the x and y values as we step along the line. If x1 is less than x2, we increment x1 by 1, otherwise we decrement it by 1
-        let sy = (y1 < y2) ? 1 : -1;
-
-        let err = dx - dy; // to determine when we need to step in the y direction (as opposed to the x direction)
-
-        while (true) {
-            const color = textures.getPixel(x1, y1, 'land');
-
-            if (color.blue > 155) {
-                return false;
-            }
-
-            if (x1 === x2 && y1 === y2) break; // we've reached the end of the line
-            let e2 = 2 * err; // calculate the error value for the next step
-            if (e2 > -dy) { err -= dy; x1 += sx; } // if the error is greater than -dy, we step in the x direction
-            if (e2 < dx) { err += dx; y1 += sy; } // if the error is less than dx, we step in the y direction
-        }
-
-        return true;
     }
 
     function create() {
@@ -136,7 +106,158 @@ document.addEventListener('DOMContentLoaded', (event) => {
                                 }
                             });
                         } else {
-                            console.log("Path crosses over the ocean, cannot move.");
+                            console.log("Path crosses over the ocean");
+
+                            let screenPath = findShorelinePath(
+                                Math.round(redCubePosition.x / scaleX),
+                                Math.round(redCubePosition.y / scaleY),
+                                x, y,
+                                originalWidth, originalHeight,
+                                width, height,
+                                this.textures
+                            );
+
+                            if (screenPath.length > 0) {
+                                // Define a recursive function to handle sequential tweening
+                                let createTween = (index) => {
+                                    if (index >= screenPath.length) return; // Exit condition for the recursion
+
+                                    const targetPosition = screenPath[index];
+                                    const distance = Phaser.Math.Distance.Between(
+                                        redCubePosition.x, redCubePosition.y, targetPosition.x, targetPosition.y
+                                    );
+                                    const speed = 150;
+                                    const duration = (distance / speed) * 1000;
+
+                                    this.tweens.add({
+                                        targets: redCubePosition,
+                                        x: targetPosition.x,
+                                        y: targetPosition.y,
+                                        duration: duration,
+                                        ease: 'Linear',
+                                        onUpdate: () => {
+                                            redCube.clear();
+                                            redCube.fillStyle(0xff0000);
+                                            redCube.fillRect(redCubePosition.x, redCubePosition.y, 50, 50);
+                                        },
+                                        onComplete: () => createTween(index + 1) // On complete, create a tween for the next point
+                                    });
+                                };
+
+                                createTween(0);
+                            } else {
+                                console.log("No valid path found");
+                            }
+
+
+                            function findShorelinePath(startX, startY, endX, endY, originalWidth, originalHeight, width, height, textures) {
+                                // Step 1: Create a 2D array representing the grid
+                                let grid = createGrid(originalWidth, originalHeight, textures);
+
+                                // Step 2: Implement A* algorithm to find the shortest path avoiding the ocean
+                                let path = aStarAlgorithm(grid, { x: startX, y: startY }, { x: endX, y: endY });
+
+                                // Step 3: Convert the path to screen coordinates
+                                let screenPath = path.map(node => ({
+                                    x: (node.x * width) / originalWidth,
+                                    y: (node.y * height) / originalHeight
+                                }));
+
+                                return screenPath;
+                            }
+
+                            function createGrid(width, height, textures) {
+                                let grid = new Array(height).fill(null).map(() => new Array(width).fill(0));
+
+                                for (let y = 0; y < height; y++) {
+                                    for (let x = 0; x < width; x++) {
+                                        let color = textures.getPixel(x, y, 'land');
+                                        grid[y][x] = color.blue > 155 ? 1 : 0;  // 1 for ocean (not walkable), 0 for land (walkable)
+                                    }
+                                }
+
+                                return grid;
+                            }
+
+                            function aStarAlgorithm(grid, start, end) {
+                                let openSet = [start];
+                                let cameFrom = grid.map(row => row.map(() => null));
+
+                                let gScores = grid.map(row => row.map(() => Infinity));
+                                gScores[start.y][start.x] = 0;
+
+                                let fScores = grid.map(row => row.map(() => Infinity));
+                                fScores[start.y][start.x] = heuristic(start, end);
+
+                                while (openSet.length > 0) {
+                                    let current = lowestFScore(openSet, fScores);
+                                    if (current.x === end.x && current.y === end.y) {
+                                        return reconstructPath(cameFrom, current);
+                                    }
+
+                                    openSet = openSet.filter(node => node.x !== current.x || node.y !== current.y);
+
+                                    for (let neighbor of getNeighbors(current, grid)) {
+                                        let tentativeGScore = gScores[current.y][current.x] + 1; // Assuming each move has a cost of 1
+                                        if (tentativeGScore < gScores[neighbor.y][neighbor.x]) {
+                                            cameFrom[neighbor.y][neighbor.x] = current;
+                                            gScores[neighbor.y][neighbor.x] = tentativeGScore;
+                                            fScores[neighbor.y][neighbor.x] = tentativeGScore + heuristic(neighbor, end);
+                                            if (!openSet.some(node => node.x === neighbor.x && node.y === neighbor.y)) {
+                                                openSet.push(neighbor);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                return []; // Return an empty array if no path was found
+                            }
+
+                            function heuristic(node, end) {
+                                return Math.abs(node.x - end.x) + Math.abs(node.y - end.y);
+                            }
+
+                            function getNeighbors(node, grid) {
+                                let neighbors = [];
+
+                                let directions = [
+                                    { dx: 1, dy: 0 },
+                                    { dx: -1, dy: 0 },
+                                    { dx: 0, dy: 1 },
+                                    { dx: 0, dy: -1 },
+                                ];
+
+                                for (let dir of directions) {
+                                    let x = node.x + dir.dx;
+                                    let y = node.y + dir.dy;
+
+                                    if (x >= 0 && y >= 0 && x < grid[0].length && y < grid.length && grid[y][x] === 0) {
+                                        neighbors.push({ x: x, y: y });
+                                    }
+                                }
+
+                                return neighbors;
+                            }
+
+                            function lowestFScore(openSet, fScores) {
+                                let lowest = openSet[0];
+                                for (let node of openSet) {
+                                    if (fScores[node.y][node.x] < fScores[lowest.y][lowest.x]) {
+                                        lowest = node;
+                                    }
+                                }
+
+                                return lowest;
+                            }
+
+                            function reconstructPath(cameFrom, current) {
+                                let path = [];
+                                while (current !== null) {
+                                    path.push(current);
+                                    current = cameFrom[current.y][current.x];
+                                }
+                                return path.reverse();
+                            }
                         }
                     }
                 } else {
@@ -163,7 +284,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     document.addEventListener('visibilitychange', () => {
         if (resumeButtonPressed || document.hidden) {
-            gamePause(document.hidden);
+            gamePause(document.hidden, game);
         }
         resumeButtonPressed = false;
     });
@@ -185,25 +306,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
         game = new Phaser.Game(config);
 
         game.canvas.id = 'battle-canvas';
-        resize();
+        resize(game);
 
         window.addEventListener('resize', () => {
-            resize();
+            resize(game);
         });
-    }
-
-    function resize() {
-        const battleScene = document.getElementById('battle-scene');
-        const gameWidth = game.config.width;
-        const gameHeight = game.config.height;
-
-        // Set the canvas dimensions to be exactly the same as the container dimensions
-        game.canvas.style.width = battleScene.offsetWidth + 'px';
-        game.canvas.style.height = battleScene.offsetHeight + 'px';
-
-        // Remove the margin settings to ensure the canvas fully covers the container
-        game.canvas.style.marginTop = '0px';
-        game.canvas.style.marginLeft = '0px';
     }
 
     playButton.addEventListener('click', () => {
@@ -212,43 +319,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
         startGame();
     });
 
-    function gamePause(isPaused) {
-        const resumeButton = document.querySelector('.resume-button');
-        
-        if (!game || game.scene.scenes[0].sceneName !== "battle-scene") {
-            console.warn('Pause function is not applicable for the current scene');
-            return;
-        }
-    
-        if (isPaused) {
-            game.loop.pause();
-            if (game.scene.scenes[0].scene.isActive()) {  // Ensure the scene is active before pausing
-                game.scene.scenes[0].scene.pause();
-            }
-            game.scene.scenes[0].tweens.pauseAll();  // Pause all active tweens
-            game.scene.scenes[0].pauseText.setVisible(true);
-            game.scene.scenes[0].input.enabled = false;
-            resumeButton.style.display = 'block';
-        } else {
-            game.loop.resume();
-            if (!game.scene.scenes[0].scene.isActive()) {  // Ensure the scene is paused before resuming
-                game.scene.scenes[0].scene.resume();
-            }
-            game.scene.scenes[0].tweens.resumeAll();  // Resume all active tweens
-            game.scene.scenes[0].input.enabled = true;
-        }
-    }
-
     resumeButton.addEventListener('click', () => {
-    resumeButtonPressed = true; // Set the flag to true when the resume button is clicked
-    game.scene.scenes[0].pauseText.setVisible(false);
-    game.scene.scenes[0].input.enabled = true;
-    if (!game.scene.scenes[0].scene.isActive()) {  // Ensure the scene is paused before resuming
-        game.scene.scenes[0].scene.resume();  // Resuming the scene when resume button is clicked
-    }
-    game.scene.scenes[0].tweens.resumeAll();  // Resume all active tweens
-    game.loop.resume();
-    resumeButton.style.display = 'none';
-});
+        resumeButtonPressed = true; // Set the flag to true when the resume button is clicked
+        game.scene.scenes[0].pauseText.setVisible(false);
+        game.scene.scenes[0].input.enabled = true;
+        if (!game.scene.scenes[0].scene.isActive()) {  // Ensure the scene is paused before resuming
+            game.scene.scenes[0].scene.resume();  // Resuming the scene when resume button is clicked
+        }
+        game.scene.scenes[0].tweens.resumeAll();  // Resume all active tweens
+        game.loop.resume();
+        resumeButton.style.display = 'none';
+    });
 
 });
