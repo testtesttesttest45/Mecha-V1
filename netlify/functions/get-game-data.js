c75303b07e3757322af36951a0f961004eaece32
testtesttesttest45 <weilinquek201@gmail.com>
2024-02-14 22:38:22 +0800

    netlify functions test 3

--- netlify/functions/get-game-data.js
+++ netlify/functions/get-game-data.js
@@ -9,8 +9,8 @@ const handler = async (event) => {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
-  const saveFilePath = path.join('/tmp', 'save_file.json');
-
+  const saveFilePath = path.join('save_file.json');
+  
   try {
     const data = await readFile(saveFilePath, 'utf8');
     return { statusCode: 200, body: data };
