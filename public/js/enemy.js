78e730648273cf5597582dfd03fe080e4b304bda
testtesttesttest45 <93181936+testtesttesttest45@users.noreply.github.com>
2024-06-21 13:37:11 +0800

    Update enemy.js

--- public/js/enemy.js
+++ public/js/enemy.js
@@ -651,7 +651,7 @@ class Enemy {
     }
 
     dropGold(causedByBaseDestruction) {
-        const goldPieces = causedByBaseDestruction ? 2 : 4;
+        const goldPieces = causedByBaseDestruction ? 1: 2;
         for (let i = 0; i < goldPieces; i++) {
             let goldX = this.sprite.x + (Math.random() * 100) - 50; // random between -50 and 50
             let goldY = this.sprite.y + (Math.random() * 100) - 50;
